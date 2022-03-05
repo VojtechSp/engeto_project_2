cba03672937772c772cfe3237369ca9c1400cffe
VojtechSp <voja.spacek@gmail.com>
2022-03-05 22:02:39 +0100

    Bulls and Cows - Full (probably)

--- Bulls and Cows.py
+++ Bulls and Cows.py
@@ -33,7 +33,6 @@ def hra(cislo):
     cislo = str(cislo)
     while vyhra == False:
         u_cislo = input()
-        check = True
         if len(u_cislo) > 4:
             print("Number input is too long!")
             break
@@ -42,32 +41,34 @@ def hra(cislo):
             break
         pocet_opakujicich_cisel = 0
         for cislo_1 in range(0,3):
+            if pocet_opakujicich_cisel > 2:
+                print("There are repeating numbers in your input!")
+                vyhra = True
+                break
             for cislo_2 in range(0,3):
                 if u_cislo[cislo_1] == u_cislo[cislo_2]:
                     pocet_opakujicich_cisel += 1
-        if pocet_opakujicich_cisel > 1:
-            print("There are repeating numbers in your input!")
-            break
-        if u_cislo.isnumeric() and u_cislo.startswith("0") == False:
-            if u_cislo == cislo:
-                print(f"Correct, you've guessed the right number\n"
-                        f"in {pokusy} guesses!\n"
-                        f"{predel}")
-                vyhra = True
+        while vyhra == False:
+            if u_cislo.isnumeric() and u_cislo.startswith("0") == False:
+                if u_cislo == cislo:
+                    print(f"Correct, you've guessed the right number\n"
+                            f"in {pokusy} guesses!\n"
+                            f"{predel}")
+                    vyhra = True
+                else:
+                    bulls = 0
+                    cows = 0
+                    for cifra in range(4):
+                        if u_cislo[cifra] == cislo[cifra]:
+                            bulls += 1
+                    for cifra in range(4):
+                        if u_cislo[cifra] in cislo and u_cislo[cifra] != cislo[cifra]:
+                            cows += 1
+                    print(f"{bulls} bulls, {cows} cows")
+                    pokusy += 1
+                    print(predel)
             else:
-                bulls = 0
-                cows = 0
-                for cifra in range(4):
-                    if u_cislo[cifra] == cislo[cifra]:
-                        bulls += 1
-                for cifra in range(4):
-                    if u_cislo[cifra] in cislo and u_cislo[cifra] != cislo[cifra]:
-                        cows += 1
-                print(f"{bulls} bulls, {cows} cows")
-                pokusy += 1
-                print(predel)
-        else:
-            print("Please input a number that doesn't start with a 0!")
+                print("Please input a number that doesn't start with a 0!")
 
 
 
